94d0c92789cbc0638eb085efc75359c71c4e2688
”Cristi <cristi.campean@hotmail.com>
2020-07-01 00:05:22 +0300

    ..

--- src/main/java/org/example/LogicalOp.java
+++ src/main/java/org/example/LogicalOp.java
@@ -2,10 +2,19 @@ package org.example;
 
 public class LogicalOp {
 
-        public void Afiseaza (int a){
-             a =10;
-        System.out.println(a);
+        public void Afiseaza (int a) {
+            a = 10;
+            System.out.println(a);
+            for (int i = 10; i > -100; i--) {
+                System.out.println(i);
 
+            }
         }
-    }
-
+            private void Numara ( int x, int y){
+                x = 10;
+                y = 20;
+                for (int i = 10; i < 20; i++) {
+                    System.out.println(i);
+                }
+            }
+        }
